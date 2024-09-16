e7e53e765d25fb865ccab828177fe9acd71968f0
Hemasai1344 <hemasai1344@gmail.com>
2024-09-16 16:02:26 +0530

    intro to python

---
+++ salary_management.py
@@ -0,0 +1,101 @@
+
+salaries = []
+"""
+
+1. add salary
+2. remove salary
+3. find avg of salaries
+4. find min of salaries1
+5. find max of salaries
+
+"""
+def add_sal(salary):
+    global salaries
+    salaries.append(salary)
+
+def rem_salary(salary):
+    global salaries
+    try:
+        salaries.remove(salary)
+    except ValueError as err:
+        print("No such salary")
+
+def sum_salaries():
+    global salaries
+    return sum(salaries)
+
+def avg_salaries():
+    global salaries
+    return sum(salaries)//len(salaries)
+
+def find_max():
+    global salaries
+    return max(salaries)
+
+def find_min():
+    global salaries
+    return min(salaries)
+
+def menu():
+    choice = int(input('''
+                   1-add
+                   2-delete
+                   3-sum
+                   4-min
+                   5-max
+                   6-avg
+                   7-exit
+                   your choice...'''))
+    
+    if choice == 1:
+        salary = float(input('enter the salary of a person '))
+        add_sal(salary)
+        print(salaries)
+    elif choice ==2:
+        salary = float(input('enter the remove salary of a person '))
+        rem_salary(salary)
+        print(salaries)
+    elif choice==3:
+        s = sum_salaries()
+        print(s)
+    elif choice == 4:
+        min = find_min()
+        print(min)
+    elif choice == 5:
+        max = find_max()
+        print(max)
+    elif choice == 6:
+        avg = avg_salaries()
+        print(avg)
+    elif choice == 7:
+        print("Exiting...")
+        return choice
+    else:
+        print("Invalid choice. Please try again.")
+    return choice
+
+def menus():
+    choice = menu()
+    while choice !=7:
+        choice = menu()
+    print('your salary automation was finished ...:)')
+menus()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- sum.py
+++ sum.py
@@ -1,3 +1,4 @@
+"""
 def find_sum(first_num,second_num):
     return first_num+second_num
 
@@ -6,3 +7,14 @@ def find_sum(first_num,second_num):
 
 s=find_sum(10,20)
 print(s)
+"""
+
+def cal(first:int, second:int)->int:
+    sum = first+second
+    diff = first-second
+    product = first*second
+    quotient = first//second
+    return sum,diff,product,quotient
+
+s,d,p,q = cal(20,6)
+print(s,d,p,q)
