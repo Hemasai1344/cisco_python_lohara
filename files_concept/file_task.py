0f3cc73d01701caf1e13498b8639323251ced03e
Hemasai1344 <hemasai1344@gmail.com>
2024-09-17 15:20:23 +0530

    ..

--- files_concept/file_task.py
+++ files_concept/file_task.py
@@ -1,17 +1,28 @@
 
 lst = list(map(str,input().split())) # [hema, sai, thaninki, cisco]
+#or lst = input().split()
+#lst = input("Enter words separeted by spaces ").split()
+
 tup = tuple(lst) # (hema, sai, thaninki, cisco)
 
+print(f'List : {lst}')
+print(f'Tuple: {tup}')
 #write both list and tuple to a text file, each on a new line
 
 with open("names.txt", "w") as file:
-    file.write("List: " + str(lst) + "\n")
-    file.write("Tuple: " + str(tup) + "\n")
+    file.write(f'List : {lst}\n')
+    file.write(f'Tuple: {tup}\n')
 
+print("Data written to file ")
 #reads the content of the file and prints them out
-
+"""
 with open('names.txt', 'r') as file:
     line1 = file.readline()
     line2 = file.readline()
     print(line1)
     print(line2)
+"""
+#or 
+with open('names.txt', 'r') as file:
+    for i in range(2):
+        print(file.readline())
